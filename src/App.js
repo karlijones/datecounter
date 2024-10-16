ea7c455a2373abc1536fd5cfe2d482264e79f76e
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-10-15 20:39:05 -0500

    export default function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
 function Counter() {
   return (
     <div 
