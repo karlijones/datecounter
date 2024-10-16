afeebf7633b80ee1e4940598c040264d4be02a16
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-10-15 21:07:44 -0500

    embedded ternary operator

--- src/App.js
+++ src/App.js
@@ -24,7 +24,13 @@ function Counter() {
         <button onClick={() => setCount(c=>c+1)}>+</button>
       </div>
       <p>
-        <span>{count === 0 ? "Today is" : ""}</span>
+        <span>
+          {count === 0 
+          ? "Today is" 
+          : count > 0 
+          ? `${count} days from today is`
+          : `${count} days ago was`}
+        </span>
       <span>{date.toDateString()}</span>
     </p>
     </div>
